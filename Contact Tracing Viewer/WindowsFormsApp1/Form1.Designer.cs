ef110b55bb1a3fb6a99759ccbf56a37f525576b5
Sofiaevx <villanueva19.sofia@gmail.com>
2021-12-02 00:24:34 +0800

    Finished Assignment

---
+++ Contact Tracing Viewer/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.buttonsearch = new System.Windows.Forms.Button();
+            this.textBoxLN = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.richTextBoxDisplay = new System.Windows.Forms.RichTextBox();
+            this.textBoxFN = new System.Windows.Forms.TextBox();
+            this.buttonreset = new System.Windows.Forms.Button();
+            this.labelLN = new System.Windows.Forms.Label();
+            this.labelFN = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.labelFN);
+            this.panel1.Controls.Add(this.labelLN);
+            this.panel1.Controls.Add(this.buttonreset);
+            this.panel1.Controls.Add(this.textBoxFN);
+            this.panel1.Controls.Add(this.buttonsearch);
+            this.panel1.Controls.Add(this.textBoxLN);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(768, 263);
+            this.panel1.TabIndex = 0;
+            // 
+            // buttonsearch
+            // 
+            this.buttonsearch.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.buttonsearch.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.buttonsearch.Font = new System.Drawing.Font("Mongolian Baiti", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonsearch.Location = new System.Drawing.Point(264, 193);
+            this.buttonsearch.Name = "buttonsearch";
+            this.buttonsearch.Size = new System.Drawing.Size(88, 26);
+            this.buttonsearch.TabIndex = 3;
+            this.buttonsearch.Text = "Search";
+            this.buttonsearch.UseVisualStyleBackColor = false;
+            this.buttonsearch.Click += new System.EventHandler(this.buttonSearch_Click);
+            // 
+            // textBoxLN
+            // 
+            this.textBoxLN.Font = new System.Drawing.Font("Mongolian Baiti", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxLN.Location = new System.Drawing.Point(12, 130);
+            this.textBoxLN.Name = "textBoxLN";
+            this.textBoxLN.Size = new System.Drawing.Size(174, 29);
+            this.textBoxLN.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Font = new System.Drawing.Font("Mongolian Baiti", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(7, 28);
+            this.label1.Name = "label1";
+            this.label1.Padding = new System.Windows.Forms.Padding(2);
+            this.label1.Size = new System.Drawing.Size(369, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Contact Tracing Information Viewer";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(382, -1);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(385, 263);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // richTextBoxDisplay
+            // 
+            this.richTextBoxDisplay.Font = new System.Drawing.Font("Mongolian Baiti", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBoxDisplay.Location = new System.Drawing.Point(12, 281);
+            this.richTextBoxDisplay.Name = "richTextBoxDisplay";
+            this.richTextBoxDisplay.Size = new System.Drawing.Size(768, 249);
+            this.richTextBoxDisplay.TabIndex = 1;
+            this.richTextBoxDisplay.Text = "";
+            // 
+            // textBoxFN
+            // 
+            this.textBoxFN.Font = new System.Drawing.Font("Mongolian Baiti", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxFN.Location = new System.Drawing.Point(192, 130);
+            this.textBoxFN.Name = "textBoxFN";
+            this.textBoxFN.Size = new System.Drawing.Size(160, 29);
+            this.textBoxFN.TabIndex = 2;
+            // 
+            // buttonreset
+            // 
+            this.buttonreset.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.buttonreset.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.buttonreset.Font = new System.Drawing.Font("Mongolian Baiti", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonreset.Location = new System.Drawing.Point(170, 193);
+            this.buttonreset.Name = "buttonreset";
+            this.buttonreset.Size = new System.Drawing.Size(88, 26);
+            this.buttonreset.TabIndex = 4;
+            this.buttonreset.TabStop = false;
+            this.buttonreset.Text = "Reset";
+            this.buttonreset.UseVisualStyleBackColor = false;
+            this.buttonreset.Click += new System.EventHandler(this.buttonreset_Click);
+            // 
+            // labelLN
+            // 
+            this.labelLN.AutoSize = true;
+            this.labelLN.Font = new System.Drawing.Font("Mongolian Baiti", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelLN.Location = new System.Drawing.Point(9, 162);
+            this.labelLN.Name = "labelLN";
+            this.labelLN.Size = new System.Drawing.Size(64, 14);
+            this.labelLN.TabIndex = 6;
+            this.labelLN.Text = "Last Name";
+            // 
+            // labelFN
+            // 
+            this.labelFN.AutoSize = true;
+            this.labelFN.Font = new System.Drawing.Font("Mongolian Baiti", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelFN.Location = new System.Drawing.Point(189, 162);
+            this.labelFN.Name = "labelFN";
+            this.labelFN.Size = new System.Drawing.Size(65, 14);
+            this.labelFN.TabIndex = 7;
+            this.labelFN.Text = "First Name";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(792, 542);
+            this.Controls.Add(this.richTextBoxDisplay);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Information Viewer";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonsearch;
+        private System.Windows.Forms.TextBox textBoxLN;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.RichTextBox richTextBoxDisplay;
+        private System.Windows.Forms.TextBox textBoxFN;
+        private System.Windows.Forms.Button buttonreset;
+        private System.Windows.Forms.Label labelFN;
+        private System.Windows.Forms.Label labelLN;
+    }
+}
+
---
+++ Contact Tracing Viewer/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        StreamReader readeroutputfile;
+        String currentline;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonSearch_Click(object sender, EventArgs e)
+        {
+            
+            string location = @"D:\This PC\Desktop\OutputFile\" + textBoxLN.Text + ", " + textBoxFN.Text + ".txt";
+            
+
+            if (File.Exists(location))
+            {
+                currentline = File.ReadAllText(location);
+                richTextBoxDisplay.Text = currentline;
+            }
+            else
+            {
+                richTextBoxDisplay.Text = "TextFile Not Found";
+            }
+        }
+
+        private void buttonreset_Click(object sender, EventArgs e)
+        {
+            textBoxLN.Text = "";
+            textBoxFN.Text = "";
+            richTextBoxDisplay.Text = "";
+        }
+    }
+}
